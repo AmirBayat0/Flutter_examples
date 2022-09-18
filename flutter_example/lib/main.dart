f40cccfbf98d07a55c26f7d509b7636d2d2c26d8
AmirBayat0 <Amirbayat.dev@gmail.com>
2022-09-18 20:01:26 +0430

    Flutter OTP Verification Screen & google_fonts package added

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
