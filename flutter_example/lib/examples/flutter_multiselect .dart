6de24fa4a3e44226c29def62722e20442810eb3b
AmirBayat0 <amirbayat.dev@gmial.com>
2023-06-22 17:27:20 +0330

    Flutter Multiselect example added

---
+++ flutter_example/lib/examples/flutter_multiselect .dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.red,
+      ),
+      home: const FinalView(),
+    );
+  }
+}
+
+class FinalView extends StatefulWidget {
+  const FinalView({Key? key}) : super(key: key);
+
+  @override
+  State<FinalView> createState() => _FinalViewState();
+}
+
+class _FinalViewState extends State<FinalView> {
+  List<String> _selectedItems = [];
+
+  void _showMultiSelect() async {
+    final List<String> items = [
+      'Flutter',
+      'Dart',
+      'Open Cv',
+      'Ruby',
+      'Next',
+      'Hive',
+      'JS',
+      'C++',
+    ];
+
+    final List<String>? results = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return MultiSelect(items: items);
+      },
+    );
+
+    if (results != null) {
+      setState(() {
+        _selectedItems = results;
+      });
+    }
+  }
+
+  void _removeFromList(String item) {
+    setState(() {
+      _selectedItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CodeWithFlexZ'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: _showMultiSelect,
+              child: const Text('Kindly Select Your Favorite Topics'),
+            ),
+            const Divider(
+              height: 30,
+            ),
+            if (_selectedItems.isEmpty)
+              const SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(height: 24),
+                    Text("There is no Topic in your Favorite List!"),
+                    SizedBox(height: 4),
+                    Text("😴✋"),
+                  ],
+                ),
+              )
+            else
+              Wrap(
+                children: _selectedItems
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 4, vertical: 2),
+                        child: Chip(
+                          label: GestureDetector(
+                            onTap: () {
+                              _removeFromList(e);
+                            },
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(e),
+                                const SizedBox(
+                                  width: 3,
+                                ),
+                                const Icon(Icons.close),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class MultiSelect extends StatefulWidget {
+  final List<String> items;
+  const MultiSelect({Key? key, required this.items}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MultiSelectState();
+}
+
+class _MultiSelectState extends State<MultiSelect> {
+  final List<String> _selectedItems = [];
+
+  void _itemChange(String itemValue, bool isSelected) {
+    setState(() {
+      if (isSelected) {
+        _selectedItems.add(itemValue);
+      } else {
+        _selectedItems.remove(itemValue);
+      }
+    });
+  }
+
+  void _cancel() {
+    Navigator.pop(context);
+  }
+
+  void _submit() {
+    Navigator.pop(context, _selectedItems);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Select Topics'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: widget.items
+              .map((item) => CheckboxListTile(
+                    value: _selectedItems.contains(item),
+                    title: Text(item),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (isChecked) => _itemChange(item, isChecked!),
+                  ))
+              .toList(),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: _cancel,
+          child: const Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: _submit,
+          child: const Text('Submit'),
+        ),
+      ],
+    );
+  }
+}
